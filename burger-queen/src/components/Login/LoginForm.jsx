c7bb39316448b53fadf83402e7300198d668bccd
Adriana Giraldo <1531nana@gmail.com>
2022-10-31 15:15:49 -0500

    validación de input, token en localStorage, rutas de navigación, iniciar sesión y productos, instalar react-router

---
+++ burger-queen/src/components/Login/LoginForm.jsx
@@ -0,0 +1,78 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+const LoginForm = () => {
+  const url = "http://localhost:8080/login";
+
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const [postResult, setPostResult] = useState([]);
+  
+  /* Cambio de rutas*/
+  const navigate = useNavigate(); 
+
+  async function postData() {
+
+    let data = {
+      email: email,
+      password: password,
+    };
+
+    console.log("data entrando", data);
+    const res = await fetch(url, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify(data),
+    });
+
+    if (res.status >= 400) {
+      alert("Validación incorrecta, intenta nuevamente");
+    } else {
+      const response = await res.json();
+      window.localStorage.setItem("loginToken", response.accessToken);
+      console.log("respuesta json ", response);
+      setPostResult(data);
+      navigate("/products");
+    }
+  }
+
+  return (
+    <form
+      action=""
+      className="loginPage_form"
+      onSubmit={(e) => {
+        e.preventDefault();
+        console.log("input enviados ", email, password);
+        setEmail("");
+        setPassword("");
+      }}
+    >
+      <input
+        type="email"
+        name="email"
+        id="email"
+        className="loginPage_input"
+        placeholder="Correo"
+        value={email}
+        onChange={(ev) => setEmail(ev.target.value)}
+      />
+      <input
+        type="password"
+        name="password"
+        id="password"
+        className="loginPage_input"
+        placeholder="Contraseña"
+        value={password}
+        onChange={(ev) => setPassword(ev.target.value)}
+      />
+      <button className="loginPage_btn" id="btnForm" onClick={postData}>
+        Iniciar Sesión
+      </button>
+    </form>
+  );
+};
+
+export default LoginForm;
---
+++ burger-queen/src/pages/login/Products/Products.jsx
@@ -0,0 +1,11 @@
+import ProductsHeaders from "../../../components/Login/Products/ProductsHeaders"
+
+const Products = () => {
+    return(
+        <div>
+            <ProductsHeaders />
+        </div>
+    )
+}
+
+export default Products;
